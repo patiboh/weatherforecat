721d8d80de3367c8fa061966fbed09b6df71b941
patiboh <lapatiboh@gmail.com>
2019-06-16 12:18:56 +0200

    ✨ Weather Forecat - Open Weather API call OK

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const OPENWEATHERMAP_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const DEFAULT_SEARCH = 'Paris,fr';
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { OPENWEATHERMAP_API_URL, DEFAULT_SEARCH } from './constants';
+
+import './styles.css';
+
+const root = document.getElementById('root');
+let searchedForText = DEFAULT_SEARCH;
+
+const displayForecat = (data) => {
+  let htmlContent = '';
+  if (data) {
+    htmlContent = `<article>
+        <p>Weather in ${searchedForText}:</p>
+        <pre>${JSON.stringify(data)}</pre>
+      </article>`;
+  } else {
+    htmlContent = '<div class="error-no-data">Sorry, no weather data available</div>';
+  }
+  const responseContainer = document.querySelector('#response-container');
+  responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+};
+
+const requestError = (error, requestCategory) => {
+  // eslint-disable-next-line
+  console.log(requestCategory + '\n' + error);
+};
+
+const init = () => {
+  root.innerHTML = `
+  <div class="main">
+    <h1>The Weather Forecat</h1>
+    <form id="search-form" action="#" class="form">
+        <label for="search-city">Choose your city</label>
+        <input id="search-city" type="text" name="search-city" placeholder="e.g. Paris" required>
+        <button id="submit-btn" type="submit" value="Submit">Submit</button>
+    </form>
+    <div id="response-container" class="container"></div>
+  </div>`;
+
+  const searchForm = document.querySelector('#search-form');
+  const searchField = document.querySelector('#search-city');
+  const responseContainer = document.querySelector('#response-container');
+
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+    const query = `${OPENWEATHERMAP_API_URL}?q=${searchedForText}&APPID=${process.env.OPENWEATHERMAP_API_KEY}`;
+    fetch(query).then(res => res.json())
+      .then(displayForecat)
+      .catch(requestError);
+  });
+};
+
+init();
+
+// This is needed for Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
