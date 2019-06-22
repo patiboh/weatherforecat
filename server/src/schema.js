e1373dd376d5b27d970f1a8d334f381716353720
patiboh <lapatiboh@gmail.com>
2019-06-22 22:11:08 +0200

    👽 Use correct weather condition codes

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,43 +1,85 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Query {
-        forecast(city: String): Forecast
-    }
+  type Query {
+    forecast(city: String): Forecast
+  }
 
-    type ForecastResponse {
-        success: Boolean!
-        forecast: Forecast
-    }
+  type ForecastResponse {
+    success: Boolean!
+    forecast: Forecast
+  }
 
-    type Location{
-        city(city: String):  String
-        country: String
-    }
+  type Location{
+    city(city: String):  String
+    country: String
+  }
 
-    type Weather {
-        temperature: Temperature!
-        conditions: CONDITIONS!
-    }
+  type Weather {
+    temperature: Temperature!
+    conditions: CONDITIONS!
+  }
 
-    type Temperature {
-        units: String
-        degrees: Float
-        min: Float
-        max: Float
-    }
+  type Temperature {
+    units: String
+    degrees: Float
+    min: Float
+    max: Float
+  }
 
-    enum CONDITIONS {
-        CLEAR
-        CLOUDS
-        RAIN
-        SNOW
-    }
+  # union PrimaryWeather = Atmosphere | Conditions
 
-    type Forecast {
-        location(city: String): Location
-        weather: Weather
-    }
+  # type Atmosphere {
+  #   code: ATMOSPHERE!
+  # }
+
+  # type Conditions {
+  #   code: CONDITIONS!
+  # }
+
+  # enum ATMOSPHERE {
+  #   MIST
+  #   SMOKE
+  #   HAZE
+  #   DUST
+  #   FOG
+  #   SAND
+  #   ASH
+  #   SQUALL
+  #   TORNADO
+  # }
+
+  # enum CONDITIONS {
+  #   THUNDERSTORM
+  #   DRIZZLE
+  #   RAIN
+  #   SNOW
+  #   CLOUDS
+  #   CLEAR
+  # }  
+
+  enum CONDITIONS {
+    THUNDERSTORM
+    DRIZZLE
+    RAIN
+    SNOW
+    CLOUDS
+    CLEAR
+    MIST
+    SMOKE
+    HAZE
+    DUST
+    FOG
+    SAND
+    ASH
+    SQUALL
+    TORNADO
+  }
+
+  type Forecast {
+    location(city: String): Location
+    weather: Weather
+  }
 `;
 
 module.exports = typeDefs;
